f3a08873c0dc3d5f3bd603f64d8c4d02e9c28dc4
thangbk2209 <thangbk2209@gmail.com>
2018-03-30 23:26:34 +0700

    fixx

--- vectorJobIDTime.py
+++ vectorJobIDTime.py
@@ -9,17 +9,17 @@ df = read_csv('/home/hunter/spark/spark-2.2.0-bin-hadoop2.7/thangbk2209/Predicti
 JobIdArr = df['jobId'].values
 numberOfJob = 0
 for jobid in JobIdArr:
-  numberOfJob += 1
-  arrVectorJobid = []
-  if numberOfJob == 284:
-    break
-  for partNumber in range(0,500):
-    file_name = string(jobid)+"_part-00"+str(partNumber).zfill(3)+"-of-00500.csv"
-    print file_name
-      if os.stat(file_name).st_size == 0:
-        arrVectorJobid.append(0)
-      else:
-        arrVectorJobid.append(1)
-  newDf = pd.DataFrame(arrVectorJobid)
+    numberOfJob += 1
+    arrVectorJobid = []
+    if numberOfJob == 284:
+        break
+    for partNumber in range(0,500):
+        file_name = string(jobid)+"_part-00"+str(partNumber).zfill(3)+"-of-00500.csv"
+        print file_name
+        if os.stat(file_name).st_size == 0:
+            arrVectorJobid.append(0)
+        else:
+            arrVectorJobid.append(1)
+    newDf = pd.DataFrame(arrVectorJobid)
 # df1 = newDf.replace(np.nan, 0, regex=True)
-  newDf.to_csv('/home/hunter/spark/spark-2.2.0-bin-hadoop2.7/thangbk2209/Predictive_Scaling/results/%s.csv'%(jobid), index=False, header=None)
+    newDf.to_csv('/home/hunter/spark/spark-2.2.0-bin-hadoop2.7/thangbk2209/Predictive_Scaling/results/%s.csv'%(jobid), index=False, header=None)
